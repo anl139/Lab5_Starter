01f90dd7270d396ab74821caee46fa064edb4a88
Andrew <anl139@ucsd.edu>
2025-05-06 10:07:34 -0700

    last minute finishes

--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,75 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+
+test('isPhoneNumber - valid formats', () => {
+  expect(isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+test('isPhoneNumber - valid formats', () => {
+  expect(isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('isPhoneNumber - invalid formats', () => {
+  expect(isPhoneNumber('1234789-0')).toBe(false);
+});
+test('isPhoneNumber - invalid formats', () => {
+  expect(isPhoneNumber('12456890')).toBe(false);
+});
+
+test('isEmail - valid emails', () => {
+  expect(isEmail('asfcda@example.com')).toBe(true);
+});
+test('isEmail - valid emails', () => {
+  expect(isEmail('anlam@domain.org')).toBe(true);
+});
+
+test('isEmail - invalid emails', () => {
+  expect(isEmail('notanemail.com')).toBe(false);
+});
+test('isEmail - invalid emails', () => {
+  expect(isEmail('notanemail.com')).toBe(false);
+});
+
+test('isStrongPassword - valid passwords', () => {
+  expect(isStrongPassword('couterofferas')).toBe(true);
+});
+test('isStrongPassword - valid passwords', () => {
+  expect(isStrongPassword('asdfaecds32')).toBe(true);
+});
+
+test('isStrongPassword - invalid passwords', () => {
+  expect(isStrongPassword('1234')).toBe(false);
+});
+test('isStrongPassword - invalid passwords', () => {
+  expect(isStrongPassword('a!@#')).toBe(false);
+});
+
+test('isDate - valid dates', () => {
+  expect(isDate('1/1/2023')).toBe(true);
+});
+test('isDate - valid dates', () => {
+  expect(isDate('12/31/2024')).toBe(true);
+});
+
+test('isDate - invalid dates', () => {
+  expect(isDate('2023/01/01')).toBe(false);
+});
+
+test('isDate - invalid dates', () => {
+  expect(isDate('131/01/2023')).toBe(false);
+});
+test('isHexColor - valid hex codes', () => {
+  expect(isHexColor('#fff')).toBe(true);
+});
+test('isHexColor - valid hex codes', () => {
+  expect(isHexColor('#A1B2C3')).toBe(true);
+});
+
+test('isHexColor - invalid hex codes', () => {
+  expect(isHexColor('#ZZZ')).toBe(false);
+});
+
+test('isHexColor - invalid hex codes', () => {
+  expect(isHexColor('1234567')).toBe(false);
+});
+
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,46 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+const synth = window.speechSynthesis;
+let voices = [];
+
+const img = document.querySelector('#explore img');
+const textInput = document.getElementById('text-to-speak');
+const voiceSelect = document.getElementById('voice-select');
+const btn = document.querySelector('#explore button');
+
+function populateVoices() {
+  voices = synth.getVoices();
+  voiceSelect.innerHTML = '';
+  voices.forEach((voice, i) => {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = `${voice.name} (${voice.lang})`;
+    voiceSelect.appendChild(option);
+  });
+}
+
 function init() {
-  // TODO
+  populateVoices();
+  if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = populateVoices;
+  }
+
+  btn.addEventListener('click', () => {
+    if (!textInput.value.trim()) return;
+    const utter = new SpeechSynthesisUtterance(textInput.value);
+    const selectedIndex = voiceSelect.value;
+    if (voices[selectedIndex]) {
+      utter.voice = voices[selectedIndex];
+    }
+
+    utter.onstart = () => {
+      img.src = 'assets/images/smiling-open.png';
+    };
+    utter.onend = () => {
+      img.src = 'assets/images/smiling.png';
+    };
+
+    synth.speak(utter);
+  });
 }
